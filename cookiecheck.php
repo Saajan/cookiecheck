62cc9ee20ef1ce4970e1886347693f11974c58e5
jathpala <jathpala@eff3a3dc-a549-0410-8461-abda4c14ae07>
2008-04-05 05:47:07 +0000

    Modified script to work as a n included script. Provides a public function cc_cookie_cutter() to manage cookie testing.

--- cookiecheck.php
+++ cookiecheck.php
@@ -2,7 +2,9 @@
 
 /* CookieCheck
  *
- * This is a simple script for testing whether the user has cookies enabled.
+ * This script provides a function for checking whether or not a visitor
+ * accessing one of your PHP scripts has cookies enabled.
+ *
  *
  * Written by Jath Palasubramaniam
  *
@@ -26,67 +28,165 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  *
- *
- * USAGE:
- *   - Edit the definitions below as desired - in particular, change
- *     CC_RETURN_PAGE to be whatever page you want to take control after this 
- *     script has run (usually the page that this script is called from).
- *   - Upload the file to your webserver
- *   - Call the script by using something similar to:
- *        header('Location: cookie_check.php');    OR
- *        header('Location: cookie_check.php?action=set');
- *     in calling page (e.g. index.php).
- *   - After completing, this script will reload the specified CC_RETURN_PAGE
- *     with either:
- *        $_GET['cookie_check'] == 'false' if cookies are disabled
- *        $_COOKIE['cookie_check'] == 'true' if cookies are enable (in the
- *          default setup of this script)
- *        $_GET['cookie_check'] == 'true' if cookies are enabled (and this
- *          script is modified to explicitly return success
- *        $_GET['cookie_check'] == 'error' if an error occured
  */
 
-define('CC_COOKIE_NAME', 'cookie_check');
-define('CC_COOKIE_EXPIRY_DAYS', 3);
+
+define('CC_QUERY', 'cc_code');
+
+define('CC_COOKIE', 'cc_test');
+define('CC_COOKIE_LIFE_DAYS', (7));
 define('CC_COOKIE_PATH', '/');
-define('CC_RETURN_PAGE', 'index.php');
-define('CC_SUCCESS_CODE', '');
-//define('CC_SUCCESS_CODE', '?cookie_check=true');
-define('CC_FAILURE_CODE', '?cookie_check=false');
-define('CC_ERROR_CODE', '?cookie_check=error');
+
+define('CC_PROTOCOL', 'http://');
+
+define('CC_SESSION_PATH', '/home/jathpala/tmp/cc_sessions');
+define('CC_SESSION_NAME', 'cc_session');
+define('CC_SESSION_ID_STEM', 'cc-sessid-');
+define('CC_SESSION_TIMEOUT', (120));
 
 
-if (isset($_GET['action'])) {
-	// Perform the action specified by the GET method variable 'action'.
-	if ($_GET['action'] == 'set') {
-		// The test cookie has not yet been set; set it and rerun this script.
-		setcookie(CC_COOKIE_NAME, 'true', time() + 
-			(CC_COOKIE_EXPIRY_DAYS * 24 * 60 * 60), CC_COOKIE_PATH);
-		header('Location: '. $_SERVER['PHP_SELF'] . '?action=get');
-	} elseif ($_GET['action'] == 'get') {
-		// The test cookie has been set; check that a cookie has been returned.
-		if (isset($_COOKIE[CC_COOKIE_NAME])) {
-			//Cookies are enabled.
-			//setcookie(CC_COOKIE_NAME, 'false', time() - (365 * 24 * 60 * 60),
-			// CC_COOKIE_PATH);
-			header('Location: ' . CC_RETURN_PAGE . CC_SUCCESS_CODE);
-			// The current implementation signals success to the return page by 
-			//  leaving the test cookie available to the return page.
-			// To explicitly signal using a GET method, change the definition of
-			//  SUCCESS_CODE to indicate a success query string.
-			// If explicitly signalling success, the setcookie() statement above
-			//  can be uncommented to force deletion of the test cookie.
-		} else {
-			//Cookies are disabled.
-			header('Location: ' . CC_RETURN_PAGE . CC_FAILURE_CODE);
+
+function _cc_initialise_session_settings() {
+
+	// Initialise the session save path
+	$old_session_settings['save_path'] = session_save_path();
+	session_save_path(session_save_path() . CC_SESSION_PATH);
+	$session_save_path = session_save_path();
+	if (!file_exists($session_save_path)) {
+		if (!mkdir($session_save_path, 0660, TRUE)) {
+			session_save_path($old_session_settings['save_path']);
+			return NULL;
 		}
 	} else {
-		// An unknown action has been specified.
-		header('Location: ' . CC_RETURN_PAGE . CC_ERROR_CODE);		;
+		if (!(is_dir($session_save_path) && is_readable($session_save_path) && is_writeable($session_save_path))) {
+			session_save_path($old_session_settings['save_path']);
+			return NULL;
+		}
 	}
-} else {
-	// Reload this page, specifing the action as set.
-	header('Location: '. $_SERVER['PHP_SELF'] . '?action=set');
+
+	// Initialise session garbage collection
+	$old_session_settings['gc_maxlifetime'] = ini_get('session.gc_maxlifetime');
+	$old_session_settings['gc_probability'] = ini_get('session.gc_probability');
+	$old_session_settings['gc_divisor'] = ini_get('session.gc_divisor');
+	ini_set('session.gc_maxlifetime', CC_SESSION_TIMEOUT);
+	ini_set('session.gc_probability', 1);
+	ini_set('session.gc_divisor', 1);
+
+	// Initialise the use of cookies for sessions
+	$old_session_settings['use_cookies'] = ini_get('session.use_cookies');
+	ini_set('session.use_cookies', 0);
+
+	// Initialise the session name
+		$session_id = session_id();
+
+	$old_session_settings['name'] = session_name();
+	session_name(CC_SESSION_NAME);
+
+	return $old_session_settings;
+
+}
+
+function _cc_restore_session_settings($old_session_settings) {
+
+	// Check that argument is valid
+	if (!is_array($old_session_settings)) {
+		return FALSE;
+	}
+
+	// Restore the session save path
+	session_save_path($old_session_settings['save_path']);
+
+	// Restore session garbage collection
+	ini_set('session.gc_maxlifetime', $old_session_settings['gc_maxlifetime']);
+	ini_set('session.gc_probability', $old_session_settings['gc_probability']);
+	ini_set('session.gc_divisor', $old_session_settings['gc_divisor']);
+
+	// Restore the use of cookies for sessions
+	ini_set('session.use_cookies', $old_session_settings['use_cookies']);
+
+	// Restore the session name
+	session_name($old_session_settings['name']);
+
+	// Restore the session id
+	session_name($old_session_settings['id']);
+
+	return TRUE;echo $_SERVER['HTTP_HOST'];
+
+}
+
+
+function _cc_save_globals() {
+
+	$_SESSION['_SESSION'] = $_SESSION;
+	$_SESSION['_SERVER'] = $_SERVER;
+	$_SESSION['_ENV'] = $_ENV;
+	$_SESSION['_COOKIE'] = $_COOKIE;
+	$_SESSION['_GET'] = $_GET;
+	$_SESSION['_POST'] = $_POST;
+	$_SESSION['_FILES'] = $_FILES;
+	$_SESSION['_REQUEST'] = $_REQUEST;
+	$_SESSION['GLOBALS'] = $GLOBALS;
+
+}
+
+
+function _cc_restore_globals() {
+
+	$_SERVER = $_SESSION['_SERVER'];
+	$_ENV = $_SESSION['_ENV'];
+	$_COOKIE = $_SESSION['_COOKIE'];
+	$_GET = $_SESSION['_GET'];
+	$_POST = $_SESSION['_POST'];
+	$_FILES = $_SESSION['_FILES'];
+	$_REQUEST = $_SESSION['_REQUEST'];
+	$GLOBALS = $_SESSION['GLOBALS'];
+	$_SESSION = $_SESSION['_SESSION'];
+
+}
+
+
+function cc_cookie_cutter() {
+
+	if (isset($_COOKIE[CC_COOKIE])) {
+
+		// Cookies are enabled - report success after restoring globals if requried
+		if (isset($_GET[CC_QUERY])) {
+			// Restore globals
+			_cc_initialise_session_settings();
+			session_id($_GET[CC_QUERY]);
+			session_start();
+			$old_session_settings = $_SESSION['old_session_settings'];
+			_cc_restore_globals;
+			session_destroy();
+			_cc_restore_session_settings($old_session_settings);
+		}
+
+		return TRUE;
+
+	} else {
+
+		// Cookies are either not enabled, or not yet sent - either send it, or report failure
+		if (!isset($_GET[CC_QUERY])) {
+			// Send a test cookie
+			$old_session_settings = _cc_initialise_session_settings();
+			session_id(CC_SESSION_ID_STEM . strval(mt_rand()));
+			$session_id = session_id();
+			session_start();
+			$address = $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
+			_cc_save_globals;
+			$_SESSION['old_session_settings'] = $old_session_settings;
+			session_write_close();
+			setcookie(CC_COOKIE, 'true', (time() + CC_COOKIE_LIFE_DAYS * 24 * 60 * 60), CC_COOKIE_PATH);
+			$header = 'Location: ' . CC_PROTOCOL . $address . '?' . CC_QUERY . '=' . $session_id;
+			header($header);
+			exit();
+		}
+
+
+		return FALSE;
+
+	}
+
 }
 
 ?>
